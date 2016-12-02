22c492d35298e818d2684e6ec5c950cc2d022bae
jfleong <jason.f.leong@gmail.com>
2016-12-02 11:19:59 -0800

    removes shit with sudo

--- osx.sh
+++ osx.sh
@@ -17,10 +17,6 @@ defaults write com.apple.finder AppleShowAllFiles YES
 killall Finder
 
 echo ""
-echo "Hide the Spotlight icon?"
-sudo chmod 600 /System/Library/CoreServices/Search.bundle/Contents/MacOS/Search
-
-echo ""
 echo "Enabling full keyboard access for all controls (enable Tab in modal dialogs, menu windows, etc.)"
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
@@ -30,15 +26,12 @@ defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 echo ""
 echo "Setting a blazingly fast keyboard repeat rate"
-defaults write NSGlobalDomain KeyRepeat -float 0.0000001
+defaults write NSGlobalDomain KeyRepeat -float 0.000000000001;
+defaults read NSGlobalDomain KeyRepeat
 
-echo ""
-echo "Disable display from automatically adjusting brightness?"
-sudo defaults write /Library/Preferences/com.apple.iokit.AmbientLightSensor "Automatic Display Enabled" -bool false
-
-echo ""
-echo "Disable keyboard from automatically adjusting backlight brightness in low light"
-sudo defaults write /Library/Preferences/com.apple.iokit.AmbientLightSensor "Automatic Keyboard Enabled" -bool false
+echo "Set a shorter Delay until key repeat"
+defaults write NSGlobalDomain InitialKeyRepeat -int 12
+defaults read NSGlobalDomain InitialKeyRepeat
 
 echo ""
 echo "Requiring password immediately after sleep or screen saver begins"
@@ -48,7 +41,6 @@ defaults write com.apple.screensaver askForPasswordDelay -int 0
 echo ""
 echo "Use column view in all Finder windows by default? (y/n)"
 defaults write com.apple.finder FXPreferredViewStyle Clmv
-
 
 echo ""
 echo "Set Dock to auto-hide and remove the auto-hiding delay? (y/n)"
